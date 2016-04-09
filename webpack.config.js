13596947779c334f80687c732a827660ca7213dc
therewasaguy <jason.sigal@gmail.com>
2016-04-09 14:50:45 -0400

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,5 @@ module.exports = {
 	resolve: {
 		'root': __dirname,
 		modulesDirectories : [path.resolve('node_modules/tone/'), path.resolve('node_modules')]
-		// 'Tone': [path.resolve('./node_modules/tone/Tone')],
-		// 'Tone/source/Player': [path.resolve('./node_modules/tone/Tone/source/Player')]
 	}
-	// resolve: {
-	// 	root: __dirname,
-	// 	modulesDirectories : ['node_modules'],
-	// }
 };
